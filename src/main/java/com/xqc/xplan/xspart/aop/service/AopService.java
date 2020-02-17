ef9a036f0011215ead8ddb98e89c5f5fab29aa17
peidong <peidong@outlook.com>
2020-02-17 14:10:01 +0800

    测试入参的传值

--- src/main/java/com/xqc/xplan/xspart/aop/service/AopService.java
+++ src/main/java/com/xqc/xplan/xspart/aop/service/AopService.java
@@ -14,5 +14,6 @@ public class AopService {
         System.out.println(this.getClass().getName()+" 开始调用");
 //        System.out.println(1/0);
         System.out.println(this.getClass().getName()+" 调用结束");
+        data.setValue("Service End Set");
     }
 }
